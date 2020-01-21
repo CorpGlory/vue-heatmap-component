b1f6f1b5bdf290a59c69e4716b195f8670f19504
corpglory-dev <47055832+corpglory-dev@users.noreply.github.com>
2020-01-21 20:21:49 +0300

    Apply suggestions from code review

--- src/components/Heatmap.vue
+++ src/components/Heatmap.vue
@@ -112,7 +112,7 @@ export default class Heatmap extends Vue {
       .call(d3.axisLeft(this.y));
   }
 
-    renderTooltip() {
+  renderTooltip() {
     this.tooltip = d3.select(`#${this.id}`)
       .append('div')
       .style('opacity', 0)
@@ -121,30 +121,30 @@ export default class Heatmap extends Vue {
       .style('border', 'solid')
       .style('border-width', '2px')
       .style('border-radius', '5px')
-      .style('padding', '5px')
+      .style('padding', '5px');
   }
 
   mouseOver(d: HeatmapData, i: number, node: any) {
     this.tooltip
-      .style('opacity', 1)
+      .style('opacity', 1);
     d3.select(node[i])
       .style('stroke', 'black')
-      .style('opacity', 1)
+      .style('opacity', 1);
   }
 
   mouseMove(d: HeatmapData, i: number, node: any) {
     this.tooltip
       .html('value: ' + d.value)
-      .style('left', (d3.mouse(node[i])[0]+10) + 'px')
-      .style('top', (d3.mouse(node[i])[1]) + 'px')
+      .style('left', (d3.mouse(node[i])[0] + 10) + 'px')
+      .style('top', (d3.mouse(node[i])[1]) + 'px');
   }
 
   mouseLeave(d: HeatmapData, i: number, node: any) {
     this.tooltip
-      .style('opacity', 0)
+      .style('opacity', 0);
     d3.select(node[i])
       .style('stroke', 'none')
-      .style('opacity', 0.8)
+      .style('opacity', 0.8);
   }
 
   renderHeatmap() {
@@ -164,9 +164,9 @@ export default class Heatmap extends Vue {
           .attr('width', this.x.bandwidth() )
           .attr('height', this.y.bandwidth() )
           .style('fill', (d: HeatmapData) => { return myColor(d.value) } )
-        .on('mouseover', this.mouseOver )
-        .on('mousemove', this.mouseMove )
-        .on('mouseleave', this.mouseLeave )
+        .on('mouseover', this.mouseOver)
+        .on('mousemove', this.mouseMove)
+        .on('mouseleave', this.mouseLeave)
   }
 
   mounted() {
