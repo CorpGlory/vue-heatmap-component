33963574cd64742206b25b8ba34dc93874611eee
vargburz <vargburz@corpglory.com>
2021-04-13 19:45:37 +0300

    tooltip upd

--- dist/vue-heatmap-component.common.js
+++ dist/vue-heatmap-component.common.js
@@ -2525,12 +2525,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"1b83aed8-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Heatmap.vue?vue&type=template&id=d1bc63a0&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"1b83aed8-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Heatmap.vue?vue&type=template&id=45aba1ae&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{attrs:{"id":_vm.id}})}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/Heatmap.vue?vue&type=template&id=d1bc63a0&
+// CONCATENATED MODULE: ./src/components/Heatmap.vue?vue&type=template&id=45aba1ae&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es.array.concat.js
 var es_array_concat = __webpack_require__("99af");
@@ -14998,7 +14998,9 @@ function (_Vue) {
         displayed: true,
         x: on_event.clientX - 40,
         y: on_event.clientY - 60,
-        content: d.value
+        content: d.value,
+        xValue: d.x,
+        yVAlue: d.y
       });
     }
   }, {
